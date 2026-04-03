ae8de7825c7d0644d7f9a78206a556d28368a20d
[elmony] <antiai.aiclub.official@gmail.com>
2026-04-04 01:32:30 +0700

    Updated src/components/PetCategoryCard.tsx

--- src/components/PetCategoryCard.tsx
+++ src/components/PetCategoryCard.tsx
@@ -28,7 +28,6 @@ const PetCategoryCard = ({ pet, onClick }: PetCategoryCardProps) => {
       {/* Pin connection line (SVG) - positioned relative to the card */}
       <div className="absolute -top-2 left-1/2 -translate-x-1/2 z-20">
         <svg width="10" height="20" viewBox="0 0 10 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <path d="M5 0V10C5 15 0 15 0 20H10C10 15 5 15 5 10V0Z" fill="black"/>
         </svg>
       </div>
 
