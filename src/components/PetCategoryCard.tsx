118ac0696647b2217feba2b4b3459d9594a1fa0e
[elmony] <antiai.aiclub.official@gmail.com>
2026-04-07 18:06:42 +0700

    [elmony] Increased the font size and icon size for the weight display in the PetCategoryCard component. - wrote 1 file(s)

--- src/components/PetCategoryCard.tsx
+++ src/components/PetCategoryCard.tsx
@@ -28,6 +28,10 @@ const PetCategoryCard = ({ pet, onClick }: PetCategoryCardProps) => {
       {/* Pin connection line (SVG) - positioned relative to the card */}
       <div className="absolute -top-2 left-1/2 -translate-x-1/2 z-20">
         <svg width="10" height="20" viewBox="0 0 10 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M5 0V10C5 15 0 15 0 20H10C10 15 5 15 5 10V0Z" fill="black"/>
+        </svg>
+        <svg className="absolute top-0 left-1/2 -translate-x-1/2" width="8" height="18" viewBox="0 0 8 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M4 0V9C4 13.5 0 13.5 0 18H8C8 13.5 4 13.5 4 9V0Z" fill="white"/>
         </svg>
       </div>
 
@@ -39,13 +43,18 @@ const PetCategoryCard = ({ pet, onClick }: PetCategoryCardProps) => {
         {/* Pet Image container */}
         <div className="relative w-[calc(100%-16px)] h-32 bg-white rounded-[1.5rem] overflow-hidden border-2 border-black mx-2 mt-2 z-10">
           <img src={pet.imageUrl} alt={pet.name} className="w-full h-full object-cover" />
+          {pet.hasHeartIcon && (
+            <div className="absolute top-2 right-2 bg-white rounded-full p-1 shadow-sm">
+              <Heart size={16} className="text-red-500 fill-red-500" />
+            </div>
+          )}
         </div>
 
         {/* Content Area */}
         <div className="p-4 relative z-10">
           <div className="flex justify-between items-center mb-2">
-            <span className="text-xs text-gray-600 font-medium flex items-center gap-1">
-              <Scale size={12} className="text-gray-500" /> {/* Scale icon added */}
+            <span className="text-sm text-gray-600 font-medium flex items-center gap-1"> {/* Changed text-xs to text-sm */}
+              <Scale size={16} className="text-gray-500" /> {/* Changed size={12} to size={16} */}
               {pet.weight} kg
             </span>
             <span className="text-xs font-bold px-3 py-1 rounded-full border border-black bg-white text-black"> {/* Increased padding for larger tag */}
