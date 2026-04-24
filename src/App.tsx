52c1a14624f2c37d28c4f529e8dbdc8dde0eeb96
[elmony] <antiai.aiclub.official@gmail.com>
2026-04-25 02:44:39 +0700

    Updated src/App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ const App = () => (
       <Toaster />
       <Sonner 
         position="bottom-center" 
-        offset={400} 
+        offset={1000} 
       />
       <BrowserRouter>
         <Routes>
