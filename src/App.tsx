a8532974110e26754d86bb9ae830a94d5705066d
[elmony] <antiai.aiclub.official@gmail.com>
2026-04-25 02:49:18 +0700

    Updated src/App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ const App = () => (
     <TooltipProvider>
       <Sonner 
         position="bottom-center" 
-        offset={120}
+        offset={200}
         toastOptions={{
           style: {
             zIndex: 99999,
