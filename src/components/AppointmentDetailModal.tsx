a14b0167b62b3c474079c4ac55e50ed60ec5d885
[elmony] <antiai.aiclub.official@gmail.com>
2026-05-06 02:11:02 +0700

    Updated src/components/AppointmentDetailModal.tsx

--- src/components/AppointmentDetailModal.tsx
+++ src/components/AppointmentDetailModal.tsx
@@ -80,7 +80,7 @@ const AppointmentDetailModal = ({ isOpen, onClose, appointment, onDelete }: Appo
               {/* 2. Pet & Status Section */}
               <div className="flex flex-col items-center py-0 gap-3  border-t border-slate-50">
                 <div className="relative">
-                  <div className="w-20 h-20 rounded-full border-[2px] border-white shadow-lg overflow-hidden bg-slate-100">
+                  <div className="w-24 h-24 rounded-full border-[2px] border-white shadow-lg overflow-hidden bg-slate-100">
                     <img 
                       src={appointment.petImage || 'https://api.dicebear.com/7.x/avataaars/svg?seed=Felix'} 
                       alt={appointment.petName} 
