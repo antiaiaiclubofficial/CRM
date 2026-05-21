cda4e78abe4b8f07ef709f3f1c371f8d46542491
[elmony] <antiai.aiclub.official@gmail.com>
2026-05-22 00:09:31 +0700

    Updated src/components/MembershipCard.tsx

--- src/components/MembershipCard.tsx
+++ src/components/MembershipCard.tsx
@@ -81,7 +81,7 @@ const MembershipCard = ({ totalAccumulatedPoints, redeemablePoints, ownerProfile
               {React.cloneElement(currentTier.icon as React.ReactElement, { fill: "currentColor" })}
               <span className="text-[9px] font-black uppercase tracking-widest">คะแนนสะสมทั้งหมด</span>
             </div>
-            <p className="text-xs font-bold text-grey leading-none tracking-tighter">{totalAccumulatedPoints.toLocaleString()}</p>
+            <p className="text-xs font-bold text-white-grey leading-none tracking-tighter">{totalAccumulatedPoints.toLocaleString()}</p>
           </div>
         </div>
 
