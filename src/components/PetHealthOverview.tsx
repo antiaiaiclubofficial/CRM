e152b97c93fa6bdb5c420dd1861dcd745a53e9b4
[elmony] <antiai.aiclub.official@gmail.com>
2026-06-10 02:49:03 +0700

    Updated src/components/PetHealthOverview.tsx

--- src/components/PetHealthOverview.tsx
+++ src/components/PetHealthOverview.tsx
@@ -271,7 +271,7 @@ const PetHealthOverview = ({
               {/* Adjusted left/right to 12px (half of w-6 dot width) to perfectly align with dot centers */}
               <div className="absolute left-[10px] right-[10px] top-[20px] -translate-y-1/2 h-1 bg-slate-100 rounded-full z-0" />
               {/* Active Progress Line - Centered vertically at 20px */}
-              <div className="absolute left-[10px] right-[10px] top-[20px] -translate-y-1/2 h-1 z-0 overflow-hidden rounded-full">
+              <div className="absolute left-[10px] right-[10px] top-[20px] -translate-y-1 h-1 z-0 overflow-hidden rounded-full">
                 <motion.div 
                   initial={{ width: 0 }}
                   animate={{ width: `${progressPercentage}%` }}
