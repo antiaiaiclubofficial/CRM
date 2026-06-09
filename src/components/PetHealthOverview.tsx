433f8bf9d40c23d09c74786d9310ec602ddf0bb3
[elmony] <antiai.aiclub.official@gmail.com>
2026-06-10 02:49:37 +0700

    Updated src/components/PetHealthOverview.tsx

--- src/components/PetHealthOverview.tsx
+++ src/components/PetHealthOverview.tsx
@@ -269,7 +269,7 @@ const PetHealthOverview = ({
             <div className="relative py-1">
               {/* Background Line - Centered vertically at 20px (8px padding-top + 12px half of dot height) */}
               {/* Adjusted left/right to 12px (half of w-6 dot width) to perfectly align with dot centers */}
-              <div className="absolute left-[10px] right-[10px] top-[20px] -translate-y-1/2 h-1 bg-slate-100 rounded-full z-0" />
+              <div className="absolute left-[10px] right-[10px] top-[20px] -translate-y-1/5 h-1 bg-slate-100 rounded-full z-0" />
               {/* Active Progress Line - Centered vertically at 20px */}
               <div className="absolute left-[10px] right-[10px] top-[20px] -translate-y-1/5 h-1 z-0 overflow-hidden rounded-full">
                 <motion.div 
