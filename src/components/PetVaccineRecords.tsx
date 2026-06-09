d09424b0b7cafc0bae7686c3fde2a0fac0dc25f8
[elmony] <antiai.aiclub.official@gmail.com>
2026-06-10 02:53:49 +0700

    Updated src/components/PetVaccineRecords.tsx

--- src/components/PetVaccineRecords.tsx
+++ src/components/PetVaccineRecords.tsx
@@ -129,7 +129,7 @@ const PetVaccineRecords = ({ data, petName, petType, onAddVaccine, onDeleteVacci
           </span>
         </div>
 
-        <div className="relative py-8">
+        <div className="relative py-4">
           {/* Background Line - Centered vertically at 30px (16px padding-top + 14px half of dot height) */}
           {/* Adjusted left/right to 14px (half of w-7 dot width) to perfectly align with dot centers */}
           <div className="absolute left-[14px] right-[14px] top-[30px] -translate-y-1/2 h-1.5 bg-slate-100 rounded-full z-0" />
